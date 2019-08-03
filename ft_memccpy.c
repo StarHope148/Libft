4e501a3df567ee4ce346f588ffe642be3fa02f9f
Jonathan <jonathan.canteau22@gmail.com>
2019-08-03 15:32:25 +0200

    edit

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: jcanteau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 16:18:39 by jcanteau          #+#    #+#             */
-/*   Updated: 2018/11/23 15:05:52 by jcanteau         ###   ########.fr       */
+/*   Updated: 2019/08/03 14:57:43 by jcanteau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	*ft_memccpy(void *dst, const void *src, int c,
 	i = -1;
 	while (++i < n)
 	{
-		ft_memcpy(&dst[i], &temp[i], 1);
+		ft_memcpy(dst + i, &temp[i], 1);
 		if (temp[i] == (unsigned char)c)
 			return (dst + i + 1);
 	}
